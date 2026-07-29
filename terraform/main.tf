f74f68bcae29c34d22a71eb5b56cc12e89f6c58d
Ajay Chinthapalli <chinthapalli.ajay@gmail.com>
2026-07-28 19:53:41 -0700

    Change scaling configuration to size 2

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,8 @@ resource "aws_eks_node_group" "workers" {
   disk_size       = null
 
   scaling_config {
-    desired_size = 3
-    min_size     = 3
-    max_size     = 3
+    desired_size = 2
+    min_size     = 2
+    max_size     = 2
   }
 }
