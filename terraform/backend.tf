19757236a5439606873f2611a0d8da82d1027085
Ajay Chinthapalli <chinthapalli.ajay@gmail.com>
2026-08-01 11:58:02 -0700

    Refactor EKS provider configuration in backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -17,14 +17,17 @@ terraform {
     encrypt        = true
   }
 }
+
 provider "aws" {
   region = var.region
 }
+
 provider "kubernetes" {
-  host                   = aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_auth.cluster.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
-data "aws_eks_auth" "cluster" {
-  name = aws_eks_cluster.this.name
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
